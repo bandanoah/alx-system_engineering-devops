66218b91c979a127d56d37ff588802c6d0844399
bandanoah <bandahnoah007@gmail.com>
2024-11-09 01:11:39 +0300

    update

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
+# The script acceps one argument from the command line and matches "School" using regex
+
+# Use ARGV[0] to get the first argument passed to the script
+# The regex `/School/` matches the word "School" exactly as specified
 puts ARGV[0].scan(/School/).join
